67c60d91bea31926a24af8f1c578271a60b33a7d
franciscoaraujo <chicoaraujo1063@gmail.com>
2024-10-19 22:41:30 -0300

    Commit

--- src/main/java/br/com/bitewisebytes/batchhello/BatchConfig.java
+++ src/main/java/br/com/bitewisebytes/batchhello/BatchConfig.java
@@ -6,14 +6,17 @@ import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -35,18 +38,25 @@ public class BatchConfig {
     @Autowired
     private Job imprimeHelloJob;
 
-
+    /**
+     * Se o Job for executado no momento da inicialização da aplicação, o método runJob() será chamado.
+     * O Job roda apenas uma vez, pois o método runJob() é chamado apenas uma vez.
+     * Se o Job for executado em outro momento, o método runJob() não será chamado.
+     * Tem tomar cuidado com isso porque se nao o Job nao vai conseguir ser reinicializados
+     * @throws Exception
+     */
 
     @PostConstruct
     public void runJob() throws Exception {
-        log.info("Iniciando Job...........");
-        JobExecution execution = jobLauncher.run(imprimeHelloJob, new JobParameters());
-        System.out.println("Job Status: " + execution.getStatus());
+        JobExecution execution = jobLauncher
+                .run(imprimeHelloJob, new JobParametersBuilder()
+                        .addString("names", "Bitewise Bytes")
+                        .toJobParameters());
+        log.info("Job Execution: " + execution.getStatus());
     }
 
     @Bean
     public Job imprimeHelloJob() {
-        log.info("JobBuilder Job...........");
         return new JobBuilder("imprimeHelloJob", jobRepository)
                 .start(imprimeHelloStep())
                 .build();
@@ -54,17 +64,21 @@ public class BatchConfig {
 
     @Bean
     public Step imprimeHelloStep() {
-        log.info("StepBuilder Step...........");
         return new StepBuilder("imprimeHelloStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("Hello, World!");
-
-                    log.info("Hello, World!...........");
-
-                    return RepeatStatus.FINISHED;
-                })
+                .tasklet(imprimeTasklet(null))
                 .transactionManager(transactionManager)
                 .build();
     }
 
+    @Bean
+    @StepScope
+    private static Tasklet imprimeTasklet(@Value("#{jobParameters['names']}") String names) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Hello, World! " + names);
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
 }
